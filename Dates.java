73927e9f130e3d85ff3576d8b85f67d2d95b9a31
Radzz123 <gundlaradhika94@gmail.com>
2020-12-22 09:54:15 +0000

    ExpiryDate

--- Dates.java
+++ Dates.java
@@ -6,22 +6,29 @@ import java.util.concurrent.*;
 public class Dates{
    public static void main(final String args[]) {
         SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
-        String addonAdditionDate1 = "02 02 2020";
-        String currentDate1 = "27 11 2020";
+        String addonAdditionDate1 = "18 12 2020";
+        String currentDate1 = "22 12 2020";
 
        try {
             Date addonAdditionDate = myFormat.parse(addonAdditionDate1);
             Date currentDate = myFormat.parse(currentDate1);
             long difference = currentDate.getTime() - addonAdditionDate.getTime();
             float daysBetween = (difference / (1000 * 60 * 60 * 24));
-            float N = daysBetween / 29;
+            double n = daysBetween / 29;
+		    if(n - Math.floor(n) > 0)
+		    {
+			    n = n + 1;
+		    }
+		
+		    int number = (int)n * 29;
+		
+		
            System.out.println("Number of Days between dates: " + daysBetween);
-           System.out.println("N : " + N);
-           int number = (int)Math.ceil(N);
-           System.out.println("Ceil : " + number);
+           System.out.println("n : " + n);
+           System.out.println("Number(N) : " + number);
            final Calendar c = Calendar.getInstance();
            c.setTime(addonAdditionDate);
-           c.add(Calendar.DATE, (number * 29));
+           c.add(Calendar.DATE, number);
            Date newRecurrenceDate= c.getTime();
            System.out.println("New Recurrence Date: "+newRecurrenceDate);
            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
